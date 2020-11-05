c50162f0d552d55aea3d791b6f3aefd3a93a7554
jay.moon <jay.moon@kakaocorp.com>
2020-11-05 11:04:01 +0900

    완주하지 못한 선수 성공 코드

--- src/main/java/com/lavinoys/code/HashExamples.java
+++ src/main/java/com/lavinoys/code/HashExamples.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class HashExamples {
     static final Logger LOGGER = LoggerFactory.getLogger(HashExamples.class);
@@ -19,6 +20,8 @@ public class HashExamples {
      * 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
      * 참가자 중에는 동명이인이 있을 수 있습니다.
      *
+     * 총 합이 홀수인 대상을 찾는다.
+     *
      * @param participant 도전자
      * @param completion 우승자
      * @return 탈락자
@@ -26,14 +29,11 @@ public class HashExamples {
     public String solution001(String[] participant, String[] completion) {
         LOGGER.info("start");
         String answer = null;
-        final Map<String, Long> partiMap = Arrays.stream(participant).parallel().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        final Map<String, Long> complMap = Arrays.stream(completion).parallel().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-
-        final Optional<Map.Entry<String, Long>> noneMatchEntry = partiMap.entrySet().stream().filter(p -> complMap.entrySet().stream().noneMatch(c -> p.getKey().equals(c.getKey()) && p.getValue().equals(c.getValue()))).findFirst();
-        if (noneMatchEntry.isPresent()) {
-            answer = noneMatchEntry.get().getKey();
+        final Map<String, Long> concatMap = Stream.concat(Arrays.stream(participant), Arrays.stream(completion)).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        final Optional<Map.Entry<String, Long>> oneEntry = concatMap.entrySet().stream().parallel().filter(map -> map.getValue() % 2 == 1).findFirst();
+        if (oneEntry.isPresent()) {
+            answer = oneEntry.get().getKey();
         }
-
         LOGGER.info("finish");
         return answer;
     }
