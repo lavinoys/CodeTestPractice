a89ef6de9f21708dbab09b2fc48d2f474afdd022
jay.moon <jay.moon@kakaocorp.com>
2020-11-05 15:25:04 +0900

    위장 테스트 코드 임시

---
+++ src/main/java/com/lavinoys/code/hash/HashExampleThird.java
@@ -0,0 +1,61 @@
+package com.lavinoys.code.hash;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class HashExampleThird {
+    static final Logger LOGGER = LoggerFactory.getLogger(HashExampleThird.class);
+
+    /**
+     * [ 위장 ]
+     *
+     * clothes의 각 행은 [의상의 이름, 의상의 종류]로 이루어져 있습니다.
+     * 스파이가 가진 의상의 수는 1개 이상 30개 이하입니다.
+     * 같은 이름을 가진 의상은 존재하지 않습니다.
+     * clothes의 모든 원소는 문자열로 이루어져 있습니다.
+     * 모든 문자열의 길이는 1 이상 20 이하인 자연수이고 알파벳 소문자 또는 '_' 로만 이루어져 있습니다.
+     * 스파이는 하루에 최소 한 개의 의상은 입습니다.
+     *
+     * 2번째 배열에 해당하는 건 한번
+     * 배일 길이 + 종류 수
+     *
+     * @param clothes 옷 목록
+     * @return 입을 수 있는 아이템 수
+     */
+    public int solution(String[][] clothes) {
+        LOGGER.info("start");
+        int answer;
+
+        List<String> categoryList = new ArrayList<>();
+        for (String[] array : clothes) {
+            categoryList.add(array[1]);
+        }
+
+        final Map<String, Long> sortMap = categoryList.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        int sumCnt = 0;
+        int sortMapSize = sortMap.size();
+        if (1 == sortMapSize) {
+            answer = sortMap.entrySet().stream().findFirst().get().getValue().intValue();
+            return answer;
+        }
+
+        int minusCnt = sortMapSize - 1;
+        for (Map.Entry<String, Long> entry : sortMap.entrySet()) {
+            sumCnt += (entry.getValue().intValue() * minusCnt);
+            if (1 < minusCnt) {
+                minusCnt--;
+            }
+        }
+        answer = sumCnt + sortMapSize;
+
+        LOGGER.info("finish");
+        return answer;
+    }
+}
--- src/test/java/com/lavinoys/code/codeUnitTests.java
+++ src/test/java/com/lavinoys/code/codeUnitTests.java
@@ -2,6 +2,7 @@ package com.lavinoys.code;
 
 import com.lavinoys.code.hash.HashExampleFirst;
 import com.lavinoys.code.hash.HashExampleSecond;
+import com.lavinoys.code.hash.HashExampleThird;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -50,6 +51,30 @@ class codeUnitTests {
 
         try {
             logger.info("return boolean : {}", hash.solution(new String[]{"119", "97674223", "1195524421"}));
+        } catch(Exception e) {
+            logger.info("Error : {}", e.getMessage());
+            logger.info("finish");
+        }
+    }
+
+    /**
+     * [ 위장 ]
+     *
+     * clothes의 각 행은 [의상의 이름, 의상의 종류]로 이루어져 있습니다.
+     * 스파이가 가진 의상의 수는 1개 이상 30개 이하입니다.
+     * 같은 이름을 가진 의상은 존재하지 않습니다.
+     * clothes의 모든 원소는 문자열로 이루어져 있습니다.
+     * 모든 문자열의 길이는 1 이상 20 이하인 자연수이고 알파벳 소문자 또는 '_' 로만 이루어져 있습니다.
+     * 스파이는 하루에 최소 한 개의 의상은 입습니다.
+     */
+    @Test
+    @DisplayName("Hash Test 3")
+    void hashTest3() {
+        Logger logger = LoggerFactory.getLogger(HashExampleThird.class);
+        HashExampleThird hash = new HashExampleThird();
+
+        try {
+            logger.info("count : {}", hash.solution(new String[][]{{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}}));
         } catch(Exception e) {
             logger.info("Error : {}", e.getMessage());
             logger.info("finish");
